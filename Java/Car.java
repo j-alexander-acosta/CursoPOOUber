a790fae50b59f493667c2197018418b2b05f826e
Alexander_Acosta <alexanderacosta@unach.cl>
2021-06-20 19:08:55 -0400

    Creacion de archivos por lenguajes

---
+++ Java/Car.java
@@ -0,0 +1,11 @@
+class Car {
+    Integer id;
+    String license;
+    String driver;
+    Integer passengenger;
+
+    void printDataCar() {
+        System.out.println("License: " + license + " Driver: " + driver);
+    }
+
+}
