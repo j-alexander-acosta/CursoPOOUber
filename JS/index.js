2b931333d57dae885943b17e19e6166e701f38cb
Alexander_Acosta <alexanderacosta@unach.cl>
2021-06-21 21:52:49 -0400

    Creacion de clases

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AWS456", new Account("Andres Herrera", "QWE234"))
 car.passenger = 4;
 car.printDataCar();
+
+var car = new UberX("AWS456", new Account("Andrea Ferran", "QWE235"), "Chevrolet", "Spark")
+car.passenger = 4;
+car.printDataCar();
--- Java/Car.java
+++ Java/Car.java
@@ -1,16 +1,34 @@
 class Car {
-    Integer id;
-    String license;
-    Account driver;
-    Integer passengenger;
+    private Integer id;
+    private String license;
+    private Account driver;
+    private Integer passegenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+        
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + " Name Driver: " + driver.name);
+        if(passegenger != null){
+            System.out.println("License: " + license + " Name Driver: " + driver.name + "Passengers: " + passegenger);    
+        }
+        g
     }
+
+    public Integer getPassenger(){
+        return passegenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if(passenger == 4){
+            this.passegenger = passenger;
+        }else {
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
+    }
+
+    
 
 }
