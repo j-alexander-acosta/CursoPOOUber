ade22509799f22bb01f75494811f2bcd8a625ac2
Alexander_Acosta <alexanderacosta@unach.cl>
2021-06-22 15:24:27 -0400

    Actualizacion de archivos

--- PHP/index.php
+++ PHP/index.php
@@ -5,8 +5,13 @@ require_once("uberPool.php");
 require_once('account.php');
 
 $uberX = new UberX("CVB123", new Account("Andres Herrera", "AMS123"), "Chevrolet", "Spark");
+$uberX->setPassenger(4);
 $uberX->printDataCar();
 
 $uberPool = new uberPool("TYU567", new Account("Andrea Ferran", "ANDA756"), "Dodge", "Attitude");
 $uberPool->printDataCar();
+
+$uberVan = new UberVan("OJL395", new Account("Raúl Ramírez", "AND456"), "Nissan", "Versa");
+$uberVan->setPassenger(6);
+$uberVan->printDataCar();
 ?>
